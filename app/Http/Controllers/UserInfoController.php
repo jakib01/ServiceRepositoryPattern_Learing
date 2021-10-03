a30456b81352d01ca0d2eecd2f508dbf6db8f2ed
jakib01 <julkernienakib@gmail.com>
2021-10-03 17:48:40 +0600

    crud code

--- app/Http/Controllers/UserInfoController.php
+++ app/Http/Controllers/UserInfoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\UserInfo;
 use Illuminate\Http\Request;
 
@@ -11,4 +12,38 @@ class UserInfoController extends Controller
         $userInfo = UserInfo::get();
         return view('welcome',compact('userInfo'));
     }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'phone_no' => 'required'
+        ]);
+
+        UserInfo::create($request->all());
+
+        return back()->with('status', 'User Info successfully added');
+    }
+
+//    public function edit(Test $test){
+//        return view('welcome', compact(''));
+//    }
+
+
+    public function update(Request $request, Test $test){
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'phone_no' => 'required'
+        ]);
+
+        $test->update($request->all());
+        return back()->with('status', 'User Info successfully Updated');
+    }
+
+        public function destory(Test $test){
+            $test->delete();
+            return back()->with('status', 'User Info successfully Delete');
+    }
+
 }
